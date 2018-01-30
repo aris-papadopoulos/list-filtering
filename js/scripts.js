6ce176def2eb441971b43ec401875ae60279ee47
Aris <aris.ppd@gmail.com>
2018-01-30 09:57:28 +0200

    Add comments

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
     var checkboxes = $(':checkbox').length;
     var listItemsLength = $('li[data-category]').length;
+    // Get Number of checkboxes and list items that exist on the document.
 
     console.log('asd', checkboxes, listItemsLength);
 
@@ -18,26 +19,34 @@ $(document).ready(function () {
                 filterSelected.push(filterName);
             } 
         }
+        // Create an array with the values of the checkboxes that are checked, every time we make a change.
 
         console.log('check', filterSelected);
         
         for (j=0; j<listItemsLength; j++) {
             var listItem = $('li[data-category]').eq(j);
             var listItemData = listItem.data('category').split(" ");
+            // Get the categories of the list item and put them in an array.
+
             var varSome = [];
             for (k=0; k<filterSelected.length; k++) {
         
                 varSome[k] = listItemData.some(function(item, index, array){
                     return item == filterSelected[k];
                 });
+                /*  Compare the array of the current list item with the array of the filters, return
+                    true or false for each element and put them in a new array.
+                */
             }
         
             console.log('varSome', varSome);
         
             varEvery = varSome.every(function(item, index, array) {
                 return item == true;
-
             })
+            /*  Applies a short of "AND" logic. If All of the categories selected by the filters
+                existed in the list item, it returns a true value.
+            */
 
             if (varEvery == true) {
                 listItem.removeClass('filterOut');
@@ -45,6 +54,7 @@ $(document).ready(function () {
             else {
                 listItem.addClass('filterOut');
             }
+            // Apply class for each element.
             console.log('varEvery', varEvery);
         }
     }); 
